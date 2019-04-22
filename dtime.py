6f4adcddc331ca43087ee821830cf97d1ee7e008
liyan <18201837008@163.com>
2019-04-22 14:54:46 +0800

    "201904221454"

--- dtime.py
+++ dtime.py
@@ -1,7 +1,12 @@
 #coding:utf-8
-import datetime
-now = datetime.datetime.now()
-s = now.strftime('%Y---%m-%d %H-%M-%S')
-y = '2018=10=10 23:23:22'
-m = datetime.datetime.strptime(y,'%Y=%m=%d %H:%M:%S')
-print(s)
+# import datetime
+# now = datetime.datetime.now()
+# s = now.strftime('%Y---%m-%d %H-%M-%S')
+# y = '2018=10=10 23:23:22'
+# m = datetime.datetime.strptime(y,'%Y=%m=%d %H:%M:%S')
+# print(s)
+import requests,json
+
+url = "http://47.107.124.228:8083/mtzx/v1/mtzx/person/getTicketOrders"
+data = {}
+requests.post()
